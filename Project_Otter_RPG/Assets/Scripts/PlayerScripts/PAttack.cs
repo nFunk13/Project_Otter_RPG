2392fececf7500677738145e64c96cb513939cc7
nFunk13 <nick.d.funk@gmail.com>
2026-05-13 00:34:58 -0500

    Fixing Storage Issue
     * Fixing issue with storing the last tile
        * Would constantly refresh itself
        * Created a bool to see if it is the first key in the key List
     * Commenting code

--- Project_Otter_RPG/Assets/Scripts/PlayerScripts/PAttack.cs
+++ Project_Otter_RPG/Assets/Scripts/PlayerScripts/PAttack.cs
@@ -8,6 +8,7 @@ public class PAttack : PlayerManager
     private KeyValuePair<int, GameObject> lastTile = new KeyValuePair<int, GameObject>();
 
     [SerializeField] List<MoveData> moves = new List<MoveData>();
+    private List<GameObject> attackTiles = new List<GameObject>();
 
     public override void Tick()
     {
@@ -17,9 +18,12 @@ public class PAttack : PlayerManager
 
     private void SeeAttackPattern()
     {
-        int keyAddition = 0;
+        // Sets up variables for setting the correct colors
+        int keyAddition = 0; // Added value to the base tile index
         GridManager gridManager = GameManager.GetInstance().GetGridManager();
         GameObject testTile = gridManager.getTileAtPosition(gridManager.MouseToWorldPosition());
+
+        // Gets the number to add to the move tile keys and makes checks
         if (testTile != null && testTile.tag == gridManager.GetEnemyTileTag())
         {
             keyAddition = gridManager.getTileKeyAtPosition(gridManager.MouseToWorldPosition());
@@ -32,26 +36,38 @@ public class PAttack : PlayerManager
         {
             keyAddition -= 1;
         }
-        int backOne = 0;
+        int backOne = 0; // Variable to move back one
 
-        foreach (GameObject tile in gridManager.GetEnemyTileDictionary().Values)
+        // Gets the tiles based on the mouse's position
+        if (moves[0].tileKeys[0] >= 1 && keyAddition <= gridManager.GetEnemyTileDictionary().Count && testTile != lastTile.Value && testTile != null)
         {
-            tile.GetComponent<SpriteRenderer>().color = Color.red;
-        }
+            attackTiles.Clear();
+            
+            // Resets tile color to red
+            foreach (GameObject tile in gridManager.GetEnemyTileDictionary().Values)
+            {
+                tile.GetComponent<SpriteRenderer>().color = Color.red;
+            }
 
-        if (moves[0].tileKeys[0] >= 1 && keyAddition <= gridManager.GetEnemyTileDictionary().Count)
-        {
+            // Sets the desired tiles to hotpink for visualization purposes
             foreach (var tileKey in gridManager.GetEnemyTileDictionary().Keys)
             {
+                bool firstTime = true; // Checks to see if the moveKey is the first one in the moves array
                 foreach (var moveKey in moves[0].tileKeys)
                 {
+                    // Sets the lastTile key value pair
+                    if (firstTime)
+                    {
+                        lastTile = new KeyValuePair<int, GameObject>((moveKey + keyAddition) + backOne, gridManager.GetEnemyTileDictionary()[(moveKey + keyAddition) + backOne]);
+                        firstTime = false;
+                    }
                     if ((keyAddition + 1) % GameManager.GetInstance().GetGridManager().GetEnemyGridWidth() == 0 && keyAddition > 0)
                     {
                         backOne = -1;
                     }
 
+                    attackTiles.Add(gridManager.GetEnemyTileDictionary()[(moveKey + keyAddition) + backOne]);
                     gridManager.GetEnemyTileDictionary()[(moveKey + keyAddition) + backOne].gameObject.GetComponent<SpriteRenderer>().color = Color.hotPink;
-                    lastTile = new KeyValuePair<int, GameObject>((moveKey + keyAddition) + backOne, gridManager.GetEnemyTileDictionary()[(moveKey + keyAddition) + backOne]);
                     continue;
                 }
             }
